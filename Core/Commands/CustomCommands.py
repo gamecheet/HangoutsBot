e7dd540ac44f8a051336e2a4414a97df3666989a
Shaun Hill <shaun.hill@gmail.com>
2015-04-03 23:18:34 -0400

    minor cleanup

--- Core/Commands/CustomCommands.py
+++ Core/Commands/CustomCommands.py
@@ -21,10 +21,6 @@ from .youtube_banlist import youtube_banlist
 def me(bot, event, *args):
     pass
 
-@DispatcherSingleton.register
-def image(bot, event, *args):
-    yield from img(bot, event, *args)
-
 @DispatcherSingleton.register_hidden
 def load_ezhiks(bot, event, *args):
     file_exception = False
@@ -72,6 +68,10 @@ def ezhik(bot, event, *args):
         print('Exception: ezhik not found (this should never happen!)')
     else:
         bot.send_image(event.conv, imageID)
+
+@DispatcherSingleton.register
+def image(bot, event, *args):
+    yield from img(bot, event, *args)
 
 @DispatcherSingleton.register
 def img(bot, event, *args):
@@ -142,7 +142,28 @@ Purpose: Shits fury all over you.
 """)
         bot.send_message_segments(event.conv, segments)
      else:
-        bot.send_message(event.conv, "What the fuck did you just fucking say about me, you little bitch? I'll have you know I graduated top of my class in the Navy Seals, and I've been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I'm the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You're fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that's just with my bare hands. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little “clever” comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn't, you didn't, and now you're paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You're fucking dead, kiddo.")
+        bot.send_message(event.conv,
+'''What the fuck did you just fucking say about me, you little bitch? \
+I'll have you know I graduated top of my class in the Navy Seals, and \
+I've been involved in numerous secret raids on Al-Quaeda, and I have over \
+300 confirmed kills. I am trained in gorilla warfare and I'm the top sniper \
+in the entire US armed forces. You are nothing to me but just another target. \
+I will wipe you the fuck out with precision the likes of which has never \
+been seen before on this Earth, mark my fucking words. You think you can \
+get away with saying that shit to me over the Internet? Think again, fucker. \
+As we speak I am contacting my secret network of spies across the USA and \
+your IP is being traced right now so you better prepare for the storm, \
+maggot. The storm that wipes out the pathetic little thing you call your \
+life. You're fucking dead, kid. I can be anywhere, anytime, and I can kill \
+you in over seven hundred ways, and that's just with my bare hands. Not only \
+am I extensively trained in unarmed combat, but I have access to the entire \
+arsenal of the United States Marine Corps and I will use it to its full \
+extent to wipe your miserable ass off the face of the continent, you little \
+shit. If only you could have known what unholy retribution your little \
+"clever" comment was about to bring down upon you, maybe you would have held \
+your fucking tongue. But you couldn't, you didn't, and now you're paying the \
+price, you goddamn idiot. I will shit fury all over you and you will drown in \
+it. You're fucking dead, kiddo.''')
 
 @DispatcherSingleton.register
 def yt(bot, event, *args):
