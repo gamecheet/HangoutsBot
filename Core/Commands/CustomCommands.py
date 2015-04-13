eb15a3441a4399d95a5a87742974b511a7a9a8cd
Shaun Hill <shaun.hill@gmail.com>
2015-04-13 00:35:06 -0400

    /webshot: refactored to use is_valid_url()

--- Core/Commands/CustomCommands.py
+++ Core/Commands/CustomCommands.py
@@ -595,45 +595,9 @@ def webshot(bot, event, *args):
         url = args[0]
         viewportsize = args[1]
 
-    # thanks to dperini and adamrofer
-    urlregex = re.compile(
-        u"^"
-        # protocol identifier
-        u"(?:(?:https?|ftp)://)"
-        # user:pass authentication
-        u"(?:\s+(?::\s*)?@)?"
-        u"(?:"
-        # ip address exclusion
-        # private & local networks
-        u"(?!(?:10|127)(?:\.\d{1,3}){3})"
-        u"(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})"
-        u"(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})"
-        # ip address dotted notation octets
-        # excludes loopback network 0.0.0.0
-        # excludes reserved space >= 224.0.0.0
-        # excludes network & broadcast addresses
-        # (first & last ip address of each class)
-        u"(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])"
-        u"(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}"
-        u"(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))"
-        u"|"
-        # host name
-        u"(?:(?:[a-z\u00a1-\uffff0-9]-?)*[a-z\u00a1-\uffff0-9]+)"
-        # domain name
-        u"(?:\.(?:[a-z\u00a1-\uffff0-9]-?)*[a-z\u00a1-\uffff0-9]+)*"
-        # tld identifier
-        u"(?:\.(?:[a-z\u00a1-\uffff]{2,}))"
-        u")"
-        # port number
-        u"(?::\d{2,5})?"
-        # resource path
-        u"(?:/\s*)?"
-        u"$"
-        , re.UNICODE)
-
-    if urlregex.match(url) is None:
+    if not is_valid_url(url):
         url = 'http://' + url
-        if urlregex.match(url) is None:
+        if not is_valid_url(url):
             bot.send_message(event.conv, "Error: invalid URL.")
             return            
 
